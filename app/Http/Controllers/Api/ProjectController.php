b42b617209a5e93de940d9f83f96402032edb7da
alessandraspalla <alespalla93@icloud.com>
2023-08-23 13:10:37 +0200

    create controller and route to send info to the frontend

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function projectApi() {
+        return response() -> json([
+            'name' => 'progetto'
+        ]);
+    }
+
+    public function projectIndex() {
+        $projects = Project :: with('type') -> paginate(5);
+
+        return response() -> json([
+            'projects' => $projects
+        ]);
+    }
 }
